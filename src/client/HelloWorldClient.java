2980450a48500982df2f5feeb2465304d7e6be53
Matthew Guizar <matthewrguizar@gmail.com>
2022-10-26 15:07:24 -0700

    transaction find/ update/delete

--- src/client/HelloWorldClient.java
+++ src/client/HelloWorldClient.java
@@ -1,19 +1,39 @@
 package client;
 
+import org.hibernate.Transaction;
 import org.hibernate.Session;
 import util.HibernateUtil;
-import domain.Message;
+import entity.Message;
 
 public class HelloWorldClient {
     public static void main(String[] args) {
         Session session = HibernateUtil.SessionFactory().openSession();
-        session.beginTransaction();
+        Transaction txn = session.getTransaction();
+        try {
+            txn.begin();
+            Message msg = (Message) session.get(Message.class, 2L);
+            // msg.setText("Hello Automatic dirty checking");
+            session.delete(msg);
+            txn.commit();
+        } catch (Exception e) {
+            if(txn != null) {
+                txn.rollback();
+                e.printStackTrace();
+            } 
+        } finally {
+            if(session!=null) {
+                session.close();
+            }
+        }
         
-        Message message = new Message("Hello world with Hibernate");
+        // session.beginTransaction();
         
-        session.save(message);
-
-        session.getTransaction().commit();
-        session.close();
+        // Message message = new Message("Hello world with Hibernate");
+        
+        // session.save(message);
+        
+        // session.getTransaction().commit();
+        // session.close();
+        System.exit(0);
     }
 }
